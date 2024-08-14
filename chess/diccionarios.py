c50ad009056f4b42d90f0ec0d484cf89a383f13c
Eyudica <e.yudica@alumno.um.edu.ar>
2024-08-14 20:07:07 -0300

    Agregar archivo de diccionarios

---
+++ chess/diccionarios.py
@@ -0,0 +1,32 @@
+BLANCO='Blanco'
+NEGRO='Negro'
+diccionarioSimbolos = {
+    BLANCO: {
+        'Peon': "♙",
+        'Torre': "♖",
+        'Caballo': "♘",
+        'Alfil': "♗",
+        'Rey': "♔",
+        'Dama': "♕"
+    },
+    NEGRO: {
+        'Peon': "♟",
+        'Torre': "♜",
+        'Caballo': "♞",
+        'Alfil': "♝",
+        'Rey': "♚",
+        'Dama': "♛"
+    }
+}
+
+diccionarioFilas={
+  "a": 0,
+  "b": 1,
+  "c": 2,
+  "d": 3,
+  "e": 4,
+  "f": 5,
+  "g": 6,
+  "h": 7,
+  
+}
