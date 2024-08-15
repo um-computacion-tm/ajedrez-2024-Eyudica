6d469e066f4c89bb9a24ee60ded9776be74015ee
Eyudica <e.yudica@alumno.um.edu.ar>
2024-08-15 09:21:40 -0300

    Eliminar archivo de test de main

--- test_chess.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from main import chess
-class TestChess(unittest.TestCase):
-    def test_chess(self):
-        self.assertEqual(chess(), "Chess")
-
-if __name__ == '__main__':
-    unittest.main()
