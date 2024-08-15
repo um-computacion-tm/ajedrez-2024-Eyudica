51d9d58f14225e4c26fbccf6ffee7d037e61bab0
Eyudica <e.yudica@alumno.um.edu.ar>
2024-08-15 09:20:24 -0300

    Actualizar archivo main

--- chess/main.py
+++ chess/main.py
@@ -1,8 +1,9 @@
-from chess.interfaz import Juego
-
+from interfaz import Juego
+import time
 CLEAR="\033[H\033[J"
 
-def main(): #Calcula el tiempo total de la partida y usa CLEAR para limpiar la pantalla, una vez que juego_finalizado de juego sea true termina la ejecucion y muestra el tiempo total
+def main(): #Calcula el tiempo total de la partida y usa CLEAR para limpiar la pantalla, una vez que juego_finalizado de juego sea true termina la ejecucion y muestra el tiempo total de partida
+    inicio=time.time()
     print(CLEAR)
     juego=Juego() 
     while not juego.juego_finalizado:
@@ -15,6 +16,8 @@ def main(): #Calcula el tiempo total de la partida y usa CLEAR para limpiar la p
     print(CLEAR)        
     juego.tablero.mostrar_tablero()               
     print(f"El ganador es {juego.ganador}")
-
+    fin=time.time()
+    total=fin-inicio
+    print(f"Tiempo total de partida: {round(total,2)} segundos")
 if __name__ == '__main__':
     main()
