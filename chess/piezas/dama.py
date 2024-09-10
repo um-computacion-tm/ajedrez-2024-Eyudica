839a2947076cadcb461de51633dd3c892e9f75ff
Eyudica <e.yudica@alumno.um.edu.ar>
2024-09-09 21:07:12 -0300

    Comentar el funcionamiento de la dama

--- chess/piezas/dama.py
+++ chess/piezas/dama.py
@@ -6,7 +6,7 @@ class Dama(Piezas):
     def __init__(self, color, posicion):
         super().__init__("Dama", color, posicion)
 
-    def checkMovimiento(self, nueva_posicion):#metodo para el movimiento de la dama, si el movimiento es diagonal, logicamente funciona como un 
+    def checkMovimiento(self, nueva_posicion):#metodo para el movimiento de la dama, si el movimiento es diagonal, logicamente funciona como un alfil.En el caso de que el movimiento es lineal, funciona logicamente como una torre
         movimiento_como_torre = Torre(self.color, self.__posicion__).checkMovimiento(nueva_posicion)
         movimiento_como_alfil = Alfil(self.color, self.__posicion__).checkMovimiento(nueva_posicion)
         if movimiento_como_torre or movimiento_como_alfil:
